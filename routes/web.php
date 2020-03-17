ebb9939f72d3d8ee447066f94948317b5d0865f9
volchenkov <d.volchenkov@dataduck.com>
2020-03-18 01:03:32 +0300

    feat: add vk auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/vk_auth', function () {
+    $query = [
+        'client_id'     => env('VK_CLIENT_ID'),
+        'redirect_uri'  => env('VK_AUTH_CALLBACK_URL'),
+        'display'       => 'page',
+        'scope'         => '65536',
+        'response_type' => 'code',
+        'state'         => env('VK_AUTH_STATE'),
+    ];
+
+    return redirect('https://oauth.vk.com/authorize?' . http_build_query($query));
+});
+
+Route::get('/vk_auth_callback', function (Request $request) {
+    if ($error = $request->query('error')) {
+        return response("{$error}: {$request->query('error_description')}", 400);
+    }
+    $code = $request->query('code');
+
+    $query = [
+        'client_id'     => env('VK_CLIENT_ID'),
+        'client_secret' => env('VK_CLIENT_SECRET'),
+        'redirect_uri'  => env('VK_AUTH_CALLBACK_URL'),
+        'code'          => $code
+    ];
+    $r = (new GuzzleHttp\Client())->get('https://oauth.vk.com/access_token', ['query' => $query]);
+
+    file_put_contents('vk_credentials.json', (string) $r->getBody());
+    if ($status = $r->getStatusCode() >= 400) {
+        return response("NOTOK: {$r->getStatusCode()}", 500);
+    }
+
+    return response('OK', 200);
 });

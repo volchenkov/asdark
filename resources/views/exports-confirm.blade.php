a26668aa4c4e554e727dd729b59e6db1d09fb90a
volchenkov <d.volchenkov@dataduck.com>
2020-06-25 00:10:58 +0300

    refactor: simplify export confirmation
    replace form with readonly link to reduce error possibility

--- resources/views/exports-confirm.blade.php
+++ resources/views/exports-confirm.blade.php
@@ -3,31 +3,15 @@
 @section('content')
 <div class="row">
     <div class="col-12">
-        <p>Внесите ID документа, по которому будут загружены объявления. Одна строка - одно объявление.</p>
-        <p>Список объявлений должен быть на листе Sheet1</p>
+        <p>Таблица создана, отредактируйте ее (одна строка - одно объявление) и начните загрузку кнопкой ниже.</p>
     </div>
 </div>
 <form action="/exports_start" method="POST">
     @csrf
     <div class="row">
-        <div class="col-10 form-group">
-            <input class="form-control"
-                   type="text"
-                   id="spreadsheetId"
-                   name="spreadsheetId"
-                   value="{{ $spreadsheetId }}"
-                   placeholder="ID документа, например 1J0Pzil0DSMKsf_HWh-uCThzLufSntclZGqKEFCl7QbB"
-                   required/>
+        <div class="col-12 form-group">
+            <a href="https://docs.google.com/spreadsheets/d/{{$spreadsheetId}}" target="_blank">https://docs.google.com/spreadsheets/d/{{$spreadsheetId}}</a>
         </div>
-        @if ($spreadsheetId)
-        <div class="col-2 form-group">
-            <a href="https://docs.google.com/spreadsheets/d/{{$spreadsheetId}}" target="_blank">
-                <button type="button" class="btn btn btn-outline-secondary">
-                    Посмотреть
-                </button>
-            </a>
-        </div>
-        @endif
     </div>
     <div class="row">
         <div class="col-2 form-group">

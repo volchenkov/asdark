6d883b23bf190a281316ea1be89cd7c6dff937da
volchenkov <d.volchenkov@dataduck.com>
2020-07-05 01:06:39 +0300

    refactor: add caveats help section

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -3,11 +3,18 @@
 @section('content')
     <div class="row">
         <div class="col-8">
-            <h3><a name="editable-fields"></a>Список редактируемых полей</h3>
+            <h4><a name="editable-fields"></a>Список редактируемых полей</h4>
             <ul>
                 @foreach($fields as $name => $field)
                     <li> <strong>{{ $name }}</strong> - {{ $field['desc'] }}</li>
                 @endforeach
+            </ul>
+
+            <h4 class="mt-5"><a name="caveats"></a>Известные ограничения редактирования</h4>
+            <ul>
+                <li>Редактирование элементов карусели поста пока не поддерживается</li>
+                <li>На данный момент в одной загрузке допускается редактирование до 2000 объявлений (до 100, если с полями постов)</li>
+                <li>Текст кнопки поста может быть не ожидаемо заменен в процессе редактирования, если он был не из списка доступных для установки через API (см. описание поля link_button, столбец "Текст" в <a href="https://vk.com/dev/wall.postAdsStealth" target="_blank">таблице</a>) </li>
             </ul>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,13 @@
 @section('content')
     <div class="row">
         <div class="col-12">
-            <p>Сервис используется для массового редактирования объявлений ВК</p>
+            <p>Сервис используется для массового редактирования объявлений ВК через Google Spreadsheets</p>
         </div>
         <div class="col-6">
             <div class="alert alert-info">
                 Для полноценной работы требуется подключение к ВК
                 <div class="small"><a href="/vk_auth_current_state">Здесь</a> можно узнать текущее состояние подключения или обновить его</div>
             </div>
-        </div>
-        <div class="col-12">
-            <a href="/ads_edit">Начать редактирование объявлений</a>
         </div>
     </div>
 @endsection

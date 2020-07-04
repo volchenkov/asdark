23643f61cb169bf6e60f46289c152be4ea991663
volchenkov <d.volchenkov@dataduck.com>
2020-07-04 19:56:06 +0300

    refactor: improve current vk state page UI

--- resources/views/vk-auth-current-state.blade.php
+++ resources/views/vk-auth-current-state.blade.php
@@ -5,9 +5,9 @@
         <div class="col-md-6 form-group">
             @if ($connection)
                 @if (isset($connection->data['account_id']))
-                    <div class="alert alert-success">ВК подключен</div>
-                    <p>Выбран {{$connection->data['account_name']}} #{{$connection->data['account_id']}}</p>
-                    <p class="small text-muted">Обновлено {{ $connection->updated_at }}</p>
+                    <p> &#128076; <strong class="text-success">ВК подключен</strong></p>
+                    <div>Выбран {{$connection->data['account_name']}} #{{$connection->data['account_id']}}</div>
+                    <p class="small text-muted">Обновлено {{ $connection->updated_at->addHours(3) }}</p>
                 @else
                     <div class="alert alert-warning">
                         При подключении ВК аккаунт не был выбран. Требуется повторить авторизацию.
@@ -21,7 +21,7 @@
 
     <div class="row">
         <div class="col-md-6 form-group">
-            <a href="/vk_auth" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true">{{ $connection ? 'Авторизоваться заново' : 'Авторизовать ВК' }}</a>
+            <a href="/vk_auth" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true">{{ $connection ? 'подключить заново' : 'подключить' }}</a>
         </div>
     </div>
 @endsection

9ef319b11d5ece69a5dd0792083cdb20d9da712a
volchenkov <d.volchenkov@dataduck.com>
2020-07-16 22:52:05 +0300

    refactor: remove embedded gss edition

--- resources/views/exports-confirm.blade.php
+++ resources/views/exports-confirm.blade.php
@@ -12,13 +12,6 @@
         <div>Открыть таблицу в новой вкладке: </div>
         <a href="https://docs.google.com/spreadsheets/d/{{$spreadsheetId}}" target="_blank">https://docs.google.com/spreadsheets/d/{{$spreadsheetId}}</a>
     </div>
-    <div class="col-12">
-        <iframe frameborder="0" style="width: 100%; height: 600px; border: 1px solid #5a5"
-                src="https://docs.google.com/spreadsheets/d/{{$spreadsheetId}}?rm=minimal">
-            Быстрое редактирование недоступно
-        </iframe>
-        <div class="text-muted small"><strong>Ctrl + F</strong> - найти, <strong>Ctrl + H</strong> - найти и заменить</div>
-    </div>
 </div>
 
 <form action="/exports_start" method="POST">

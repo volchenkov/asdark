d394040d4a7bf70c245a2e6d6904f9305c249477
volchenkov <d.volchenkov@dataduck.com>
2020-09-27 23:09:21 +0300

    feat: fix navbar width for solid, consistent UI

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -54,33 +54,35 @@
 <body>
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <button class="navbar-toggler collapsed"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarTop"
-            aria-controls="navbarTop"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="navbar-collapse collapse" id="navbarTop" style="">
-        <ul class="navbar-nav mr-auto">
-            <a href="/" class="navbar-brand"> <strong>ASDARK</strong></a>
-            <li class="nav-item">
-                <a href="{{ route('adsEdit.start') }}"
-                   class="nav-link {{ request()->is('ads_edit*') ? 'active' : '' }}">Редактирование объявлений</a>
-            </li>
-            <li class="nav-item">
-                <a href="/exports"
-                   class="nav-link {{ request()->is('export*') ? 'active' : '' }}">Загрузки</a>
-            </li>
-        </ul>
+    <div class="container">
+        <button class="navbar-toggler collapsed"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarTop"
+                aria-controls="navbarTop"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarTop" style="">
+            <ul class="navbar-nav mr-auto">
+                <a href="/" class="navbar-brand"> <strong>ASDARK</strong></a>
+                <li class="nav-item">
+                    <a href="{{ route('adsEdit.start') }}"
+                       class="nav-link {{ request()->is('ads_edit*') ? 'active' : '' }}">редактирование объявлений</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/exports"
+                       class="nav-link {{ request()->is('export*') ? 'active' : '' }}">загрузки</a>
+                </li>
+            </ul>
 
-        <ul class="navbar-nav ml-md-auto">
-            <li class="nav-item">
-                <a href="/logout" class="nav-link">Выход</a>
-            </li>
-        </ul>
+            <ul class="navbar-nav ml-md-auto">
+                <li class="nav-item">
+                    <a href="/logout" class="nav-link">выход</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 <main role="main" class="container mt-4">
@@ -90,9 +92,9 @@
 <footer class="footer">
     <div class="container">
         <a href="/help"
-           class="mr-3 text-muted {{ (request()->is('help*')) ? 'text-underlined' : '' }}">Справка</a>
+           class="mr-3 text-muted {{ (request()->is('help*')) ? 'text-underlined' : '' }}">справка</a>
         <a href="/vk_auth_current_state"
-           class="text-muted {{ (request()->is('vk_auth*')) ? 'text-underlined' : '' }}">Подключение ВК</a>
+           class="text-muted {{ (request()->is('vk_auth*')) ? 'text-underlined' : '' }}">подключение ВК</a>
     </div>
 </footer>
 </body>
